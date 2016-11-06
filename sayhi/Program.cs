8267a5c04c723d807926edc981048d781999f638
robinlyang <robiny@gmail.com>
2016-11-05 22:11:12 -0600

    Initial Project

--- sayhi/Program.cs
+++ sayhi/Program.cs
@@ -10,6 +10,29 @@ namespace sayhi
     {
         static void Main(string[] args)
         {
+            String name, number1, number2;
+
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hi there " + name);
+            Console.Write("Enter hours worked: ");
+            number1 = Console.ReadLine();
+            Console.Write("Enter rate per hour: ");
+            number2 = Console.ReadLine();
+            double total, x, y;
+            x = Convert.ToDouble(number1);
+            y = Convert.ToDouble(number2);
+            if(x <= 40)
+            {
+                total = x * y;
+            }
+            else
+            {
+                total = x * y * 1.5;
+            }
+            
+            Console.WriteLine("Total pay is " + total.ToString("c"));
+            Console.ReadLine();
         }
     }
 }
